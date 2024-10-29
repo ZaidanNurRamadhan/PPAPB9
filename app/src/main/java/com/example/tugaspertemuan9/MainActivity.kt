fdf810b5954562aa3e204f516169114200c6b837
ZaidanNurRamadhan <zaidannoer441@gmail.com>
2024-10-29 23:18:40 +0700

    tugas

---
+++ app/src/main/java/com/example/tugaspertemuan9/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.tugaspertemuan9
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.tugaspertemuan9.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Inflate the layout using View Binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Enable edge-to-edge display
+        enableEdgeToEdge()
+
+        // Set up the NavController for the BottomNavigationView
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+    }
+}
